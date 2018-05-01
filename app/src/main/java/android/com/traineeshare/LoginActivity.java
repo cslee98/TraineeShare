0bfc02b35b5567c8598ccb45d25fbac6e462bb42
JoshTan <kaiweitan128@gmail.com>
2018-05-01 18:27:04 +0800

    Version 1.1
    Signed-off-by: JoshTan <kaiweitan128@gmail.com>

--- app/src/main/java/android/com/traineeshare/LoginActivity.java
+++ app/src/main/java/android/com/traineeshare/LoginActivity.java
@@ -1,12 +1,24 @@
 package android.com.traineeshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button btn_Login = findViewById(R.id.btn_Login);
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
     }
 }
