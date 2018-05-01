54930630f4af4282fbee76bc9352983881227fda
JoshTan <kaiweitan128@gmail.com>
2018-05-01 22:22:10 +0800

    Version 1.1
    Signed-off-by: JoshTan <kaiweitan128@gmail.com>

--- app/src/main/java/android/com/traineeshare/LoginActivity.java
+++ app/src/main/java/android/com/traineeshare/LoginActivity.java
@@ -3,8 +3,11 @@ package android.com.traineeshare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -12,7 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final Button btn_Login = findViewById(R.id.btn_Login);
+        final EditText et_email = findViewById(R.id.et_email);
+        final EditText et_pass = findViewById(R.id.et_password);
+        final CardView btn_Login = findViewById(R.id.btn_Login);
+
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,5 +26,7 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+        et_email.getText();
+        et_pass.getText();
     }
 }
